d57e4859d99a0a0ae6269887e094625debc4f3df
Lucca Martins <lucca_martins30@yahoo.com.br>
2025-09-03 09:40:20 -0300

    fix lint

--- internal/ethapi/simulate.go
+++ internal/ethapi/simulate.go
@@ -355,6 +355,7 @@ func (sim *simulator) processBlock(ctx context.Context, block *simBlock, header,
 	blockBody := &types.Body{Transactions: txes, Withdrawals: *block.BlockOverrides.Withdrawals}
 	chainHeadReader := &simChainHeadReader{ctx, sim.b}
 	b, receipts, err := sim.b.Engine().FinalizeAndAssemble(chainHeadReader, header, sim.state, blockBody, receipts)
+	_ = receipts // mark unused
 	if err != nil {
 		return nil, nil, nil, err
 	}
