c39cbcd9d5220960dc9c514fb00b1fbce5f101de
Angel Valkov <avalkov@polygon.technology>
2025-09-19 14:26:37 +0300

    Use New instead of NewWithReader for serial execution

--- core/blockchain.go
+++ core/blockchain.go
@@ -658,7 +658,7 @@ func (bc *BlockChain) ProcessBlock(block *types.Block, parent *types.Header, wit
 	if err != nil {
 		return nil, nil, 0, nil, 0, err
 	}
-	statedb, err := state.NewWithReader(parentRoot, bc.statedb, process)
+	statedb, err := state.New(parentRoot, bc.statedb)
 	if err != nil {
 		return nil, nil, 0, nil, 0, err
 	}
