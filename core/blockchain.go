2222bed8f1eaaf7d0594f7489c1f5be5fbba9ce8
Angel Valkov <avalkov@polygon.technology>
2025-10-03 17:54:37 +0300

    Allow committing block

--- core/blockchain.go
+++ core/blockchain.go
@@ -2840,7 +2840,7 @@ func (bc *BlockChain) insertChainWithWitnesses(chain types.Blocks, setHead bool,
 			return nil, it.index, whitelist.ErrMismatch
 		}
 
-		panic("Wont commit block")
+		// panic("Wont commit block")
 
 		if !setHead {
 			// Don't set the head, only insert the block
