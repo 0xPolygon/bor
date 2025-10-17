25c556915313a35f927664ce75fe7a0b3f30e247
Raneet Debnath <raneetdebnath10@gmail.com>
2025-10-17 16:41:58 +0530

    core: track state root calc vs block exec

--- core/blockchain_insert.go
+++ core/blockchain_insert.go
@@ -30,6 +30,8 @@ type insertStats struct {
 	queued, processed, ignored int
 	usedGas                    uint64
 	startTime                  mclock.AbsTime
+	execDur                    time.Duration
+	stateCalcDur               time.Duration
 }
 
 // statsReportLimit is the time limit during import and export after which we
@@ -60,6 +62,12 @@ func (st *insertStats) report(chain []*types.Block, index int, snapDiffItems, sn
 			"number", end.Number(), "hash", end.Hash(),
 			"blocks", st.processed, "txs", txs, "mgas", float64(st.usedGas) / 1000000,
 			"elapsed", common.PrettyDuration(elapsed), "mgasps", mgasps,
+		}
+		if st.execDur != 0 {
+			context = append(context, []interface{}{"exec", common.PrettyDuration(st.execDur)}...)
+		}
+		if st.stateCalcDur != 0 {
+			context = append(context, []interface{}{"statecalc", common.PrettyDuration(st.stateCalcDur)}...)
 		}
 		if timestamp := time.Unix(int64(end.Time()), 0); time.Since(timestamp) > time.Minute {
 			context = append(context, []interface{}{"age", common.PrettyAge(timestamp)}...)
