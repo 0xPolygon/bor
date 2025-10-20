f53169de2b963326dc65d8e542d035f36f873ea2
Lucca Martins <lucca_martins30@yahoo.com.br>
2025-10-20 11:46:18 -0300

    fix block pruner cursor key

--- core/rawdb/schema.go
+++ core/rawdb/schema.go
@@ -120,7 +120,7 @@ var (
 
 	blockBodyPrefix     = []byte("b") // blockBodyPrefix + num (uint64 big endian) + hash -> block body
 	blockReceiptsPrefix = []byte("r") // blockReceiptsPrefix + num (uint64 big endian) + hash -> block receipts
-	BlockPruneCursorKey = []byte("witnessPruneCursorKey")
+	BlockPruneCursorKey = []byte("blockPruneCursorKey")
 
 	txLookupPrefix        = []byte("l") // txLookupPrefix + hash -> transaction/receipt lookup metadata
 	bloomBitsPrefix       = []byte("B") // bloomBitsPrefix + bit (uint16 big endian) + section (uint64 big endian) + hash -> bloom bits
