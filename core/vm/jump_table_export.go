087e8634d8a90b28a64629471ee46c5e7b50778e
marcello33 <marcelloardizzone@hotmail.it>
2025-10-28 17:59:07 +0100

    fix lint

--- core/vm/jump_table_export.go
+++ core/vm/jump_table_export.go
@@ -30,7 +30,7 @@ func LookupInstructionSet(rules params.Rules) (JumpTable, error) {
 		return newCancunInstructionSet(), errors.New("verkle-fork not defined yet")
 	case rules.IsMadhugiri:
 		return newPragueInstructionSet(), errors.New("madhugiri-fork not defined yet")
-		case rules.IsOsaka:
+	case rules.IsOsaka:
 		return newPragueInstructionSet(), errors.New("osaka-fork not defined yet")
 	case rules.IsPrague:
 		return newPragueInstructionSet(), nil
