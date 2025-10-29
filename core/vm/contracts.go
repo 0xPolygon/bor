68fbcd0040e312097ed12c73711befe4e9fb2c04
marcello33 <marcelloardizzone@hotmail.it>
2025-10-29 12:15:53 +0100

    split precompiled contracts and addresses for madhugiri HF from osaka

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -164,7 +164,30 @@ var PrecompiledContractsOsaka = PrecompiledContracts{
 	common.BytesToAddress([]byte{0x11}): &bls12381MapG2{},
 }
 
+// PrecompiledContractsMadhugiri contains the set of pre-compiled Ethereum
+// contracts used in the Madhugiri release (bor HF).
+var PrecompiledContractsMadhugiri = PrecompiledContracts{
+	common.BytesToAddress([]byte{0x01}): &ecrecover{},
+	common.BytesToAddress([]byte{0x02}): &sha256hash{},
+	common.BytesToAddress([]byte{0x03}): &ripemd160hash{},
+	common.BytesToAddress([]byte{0x04}): &dataCopy{},
+	common.BytesToAddress([]byte{0x05}): &bigModExp{eip2565: true, eip7823: true, eip7883: true},
+	common.BytesToAddress([]byte{0x06}): &bn256AddIstanbul{},
+	common.BytesToAddress([]byte{0x07}): &bn256ScalarMulIstanbul{},
+	common.BytesToAddress([]byte{0x08}): &bn256PairingIstanbul{},
+	common.BytesToAddress([]byte{0x09}): &blake2F{},
+	common.BytesToAddress([]byte{0x0a}): &kzgPointEvaluation{},
+	common.BytesToAddress([]byte{0x0b}): &bls12381G1Add{},
+	common.BytesToAddress([]byte{0x0c}): &bls12381G1MultiExp{},
+	common.BytesToAddress([]byte{0x0d}): &bls12381G2Add{},
+	common.BytesToAddress([]byte{0x0e}): &bls12381G2MultiExp{},
+	common.BytesToAddress([]byte{0x0f}): &bls12381Pairing{},
+	common.BytesToAddress([]byte{0x10}): &bls12381MapG1{},
+	common.BytesToAddress([]byte{0x11}): &bls12381MapG2{},
+}
+
 var (
+	PrecompiledAddressesMadhugiri []common.Address
 	PrecompiledAddressesOsaka     []common.Address
 	PrecompiledAddressesPrague    []common.Address
 	PrecompiledAddressesCancun    []common.Address
@@ -204,7 +227,7 @@ func init() {
 func activePrecompiledContracts(rules params.Rules) PrecompiledContracts {
 	switch {
 	case rules.IsMadhugiri:
-		return PrecompiledContractsOsaka
+		return PrecompiledContractsMadhugiri
 	case rules.IsVerkle:
 		return PrecompiledContractsVerkle
 	case rules.IsOsaka:
@@ -233,7 +256,7 @@ func ActivePrecompiledContracts(rules params.Rules) PrecompiledContracts {
 func ActivePrecompiles(rules params.Rules) []common.Address {
 	switch {
 	case rules.IsMadhugiri:
-		return PrecompiledAddressesOsaka
+		return PrecompiledAddressesMadhugiri
 	case rules.IsOsaka:
 		return PrecompiledAddressesOsaka
 	case rules.IsPrague:
