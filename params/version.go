23c233254fce94274441cb45eea5ae708b1a9eea
marcello33 <marcelloardizzone@hotmail.it>
2025-11-03 09:39:53 +0100

    params: bump version to v2.4.1-beta

--- params/version.go
+++ params/version.go
@@ -24,9 +24,9 @@ import (
 
 const (
 	VersionMajor = 2  // Major version component of the current release
-	VersionMinor = 3  // Minor version component of the current release
-	VersionPatch = 3  // Patch version component of the current release
-	VersionMeta  = "" // Version metadata to append to the version string
+	VersionMinor = 4  // Minor version component of the current release
+	VersionPatch = 1  // Patch version component of the current release
+	VersionMeta  = "beta" // Version metadata to append to the version string
 )
 
 var (
