3f34ff88f715cd1076abd32f64b5f405d19b69d6
kamuikatsurgi <shahkrishang11@gmail.com>
2025-11-14 13:05:24 +0530

    chore: add precompile logs

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -26,6 +26,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
 )
@@ -234,7 +235,28 @@ func (evm *EVM) Call(caller common.Address, addr common.Address, input []byte, g
 	evm.Context.Transfer(evm.StateDB, caller, addr, value)
 
 	if isPrecompile {
-		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		// Log modexp precompile invocation and completion (address 0x05)
+		if addr == common.BytesToAddress([]byte{0x05}) {
+			startGas := gas
+			log.Info("MODEXP precompile called",
+				"block", evm.Context.BlockNumber,
+				"time", evm.Context.Time,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"from", caller,
+				"type", "CALL",
+				"gas", gas,
+			)
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+			usedGas := startGas - gas
+			log.Info("MODEXP precompile finished",
+				"block", evm.Context.BlockNumber,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"usedGas", usedGas,
+				"error", err,
+			)
+		} else {
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		}
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
 		code := evm.resolveCode(addr)
@@ -301,7 +323,28 @@ func (evm *EVM) CallCode(caller common.Address, addr common.Address, input []byt
 
 	// It is allowed to call precompiles, even via delegatecall
 	if p, isPrecompile := evm.precompile(addr); isPrecompile {
-		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		// Log modexp precompile invocation and completion (address 0x05)
+		if addr == common.BytesToAddress([]byte{0x05}) {
+			startGas := gas
+			log.Info("MODEXP precompile called",
+				"block", evm.Context.BlockNumber,
+				"time", evm.Context.Time,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"from", caller,
+				"type", "CALLCODE",
+				"gas", gas,
+			)
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+			usedGas := startGas - gas
+			log.Info("MODEXP precompile finished",
+				"block", evm.Context.BlockNumber,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"usedGas", usedGas,
+				"error", err,
+			)
+		} else {
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		}
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
 		// The contract is a scoped environment for this execution context only.
@@ -347,7 +390,28 @@ func (evm *EVM) DelegateCall(originCaller common.Address, caller common.Address,
 
 	// It is allowed to call precompiles, even via delegatecall
 	if p, isPrecompile := evm.precompile(addr); isPrecompile {
-		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		// Log modexp precompile invocation and completion (address 0x05)
+		if addr == common.BytesToAddress([]byte{0x05}) {
+			startGas := gas
+			log.Info("MODEXP precompile called",
+				"block", evm.Context.BlockNumber,
+				"time", evm.Context.Time,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"from", caller,
+				"type", "DELEGATECALL",
+				"gas", gas,
+			)
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+			usedGas := startGas - gas
+			log.Info("MODEXP precompile finished",
+				"block", evm.Context.BlockNumber,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"usedGas", usedGas,
+				"error", err,
+			)
+		} else {
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		}
 	} else {
 		// Initialise a new contract and make initialise the delegate values
 		//
@@ -402,7 +466,28 @@ func (evm *EVM) StaticCall(caller common.Address, addr common.Address, input []b
 	evm.StateDB.AddBalance(addr, new(uint256.Int), tracing.BalanceChangeTouchAccount)
 
 	if p, isPrecompile := evm.precompile(addr); isPrecompile {
-		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		// Log modexp precompile invocation and completion (address 0x05)
+		if addr == common.BytesToAddress([]byte{0x05}) {
+			startGas := gas
+			log.Info("MODEXP precompile called",
+				"block", evm.Context.BlockNumber,
+				"time", evm.Context.Time,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"from", caller,
+				"type", "STATICCALL",
+				"gas", gas,
+			)
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+			usedGas := startGas - gas
+			log.Info("MODEXP precompile finished",
+				"block", evm.Context.BlockNumber,
+				"txIndex", evm.StateDB.Inner().TxIndex(),
+				"usedGas", usedGas,
+				"error", err,
+			)
+		} else {
+			ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		}
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
 		// The contract is a scoped environment for this execution context only.
