983aff0bec1c7e862ed0dc477ca7de3495bf7f9f
Manav Darji <manavdarji.india@gmail.com>
2025-11-19 15:48:12 +0530

    core/txpool/legacypool: add metrics in txpool.add for lock observability

--- core/txpool/legacypool/legacypool.go
+++ core/txpool/legacypool/legacypool.go
@@ -131,6 +131,14 @@ var (
 	// pendingWaitTimer measures the total time taken for a pending call. This is useful
 	// to understand delay in block building.
 	pendingWaitTimer = metrics.NewRegisteredTimer("txpool/pendingwait", nil)
+
+	// metrics to capture time taken in adding transactions
+	syncAddTimer                = metrics.NewRegisteredTimer("txpool/add/sync", nil)
+	asyncAddStage0Timer         = metrics.NewRegisteredTimer("txpool/add/stage0", nil)
+	asyncAddStage1Timer         = metrics.NewRegisteredTimer("txpool/add/stage1", nil)
+	asyncAddStage2Timer         = metrics.NewRegisteredTimer("txpool/add/stage2", nil)
+	asyncAddStage0LockWaitTimer = metrics.NewRegisteredTimer("txpool/add/stage0lockwait", nil)
+	asyncAddStage2LockWaitTimer = metrics.NewRegisteredTimer("txpool/add/stage2lockwait", nil)
 )
 
 // BlockChain defines the minimal set of methods needed to back a tx pool with
@@ -749,6 +757,30 @@ func (pool *LegacyPool) validateAuth(tx *types.Transaction) error {
 	return nil
 }
 
+// reportTxAddMetrics updates metrics captured in async tx addition.
+func reportTxAddMetrics(stage uint8, stage0Duration, stage1Duration, stage2Duration time.Duration) {
+	if stage > 2 {
+		return
+	}
+	// default case in all stages
+	if stage1Duration > 0 {
+		asyncAddStage0Timer.Update(stage0Duration)
+	}
+	switch stage {
+	case 1:
+		if stage1Duration > 0 {
+			asyncAddStage1Timer.Update(stage1Duration)
+		}
+	case 2:
+		if stage1Duration > 0 {
+			asyncAddStage1Timer.Update(stage1Duration)
+		}
+		if stage2Duration > 0 {
+			asyncAddStage2Timer.Update(stage2Duration)
+		}
+	}
+}
+
 // add validates a transaction and inserts it into the non-executable queue for later
 // pending promotion and execution. If the transaction is a replacement for an already
 // pending or queued one, it overwrites the previous transaction if its price is higher.
@@ -756,22 +788,59 @@ func (pool *LegacyPool) validateAuth(tx *types.Transaction) error {
 // functions to use the pending pool. It also performs some operations on the `pricedHeap`
 // async to avoid waiting for reheap. The pool lock won't be held when called in async mode.
 func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, err error) {
-	// If `async` is set, acquire the global lock.
 	var locked bool = true
-	if async {
-		pool.mu.Lock()
+
+	// For sync mode (i.e. async=false), lock is held in outer function so capture the
+	// start and end of function directly.
+	if !async {
+		syncAddTime := time.Now()
 		defer func() {
+			syncAddTimer.Update(time.Since(syncAddTime))
+		}()
+	}
+
+	// We split this function into 3 major stages. To avoid unexpected issues, the stages aren't
+	// split into separate functions as the only purpose is capturing time spent in each stage.
+	// Broadly, this function will either be called in sync mode (async=false) which has the lock
+	// acquired for the entire duration, or in async mode (async=true) where lock is acquired
+	// initially, released for a specific code path and re-acquired later. Changes needed to track
+	// time taken in different code path are bit messy but prevents any behavioral changes.
+	// Stage0: Defines the initial validation of the transaction (lock is held)
+	// Stage1: Checking against the priced list for tx pricing (lock is released and re-acquired)
+	// Stage2: Re-arranging txpool contents for inclusion/exclusion (lock is held)
+	var (
+		currentStage   uint8 = 0
+		stage0Time     time.Time
+		stage0Duration time.Duration
+		stage1Time     time.Time
+		stage1Duration time.Duration
+		stage2Time     time.Time
+		stage2Duration time.Duration
+	)
+
+	// If `async` is set, acquire the global lock.
+	if async {
+		lockStart := time.Now()
+		pool.mu.Lock()
+		asyncAddStage0LockWaitTimer.Update(time.Since(lockStart))
+		defer func() {
+			// Based on which stage the code exits, report metrics accordingly.
+			reportTxAddMetrics(currentStage, stage0Duration, stage1Duration, stage2Duration)
 			if locked {
 				pool.mu.Unlock()
 			}
 		}()
 	}
 
+	// stage0 starts
+	stage0Time = time.Now()
+
 	// If the transaction is already known, discard it
 	hash := tx.Hash()
 	if pool.all.Get(hash) != nil {
 		log.Trace("Discarding already known transaction", "hash", hash)
 		knownTxMeter.Mark(1)
+		stage0Duration = time.Since(stage0Time)
 		return false, txpool.ErrAlreadyKnown
 	}
 
@@ -783,6 +852,7 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 	if err := pool.validateTx(tx); err != nil {
 		log.Trace("Discarding invalid transaction", "hash", hash, "err", err)
 		invalidTxMeter.Mark(1)
+		stage0Duration = time.Since(stage0Time)
 		return false, err
 	}
 	// already validated by this point
@@ -796,6 +866,7 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 	)
 	if !hasPending && !hasQueued {
 		if err := pool.reserver.Hold(from); err != nil {
+			stage0Duration = time.Since(stage0Time)
 			return false, err
 		}
 		defer func() {
@@ -810,6 +881,14 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 			}
 		}()
 	}
+
+	// stage0 ends, update final duration, increment stage
+	stage0Duration = time.Since(stage0Time)
+	currentStage = 1
+
+	// start stage2 incase stage1 never runs (due to the if below)
+	stage2Time = time.Now()
+
 	// If the transaction pool is full, discard underpriced transactions
 	if uint64(pool.all.Slots()+numSlots(tx)) > pool.config.GlobalSlots+pool.config.GlobalQueue {
 		if async {
@@ -818,10 +897,15 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 			pool.mu.Unlock()
 			locked = false
 		}
+
+		// stage1 starts
+		stage1Time = time.Now()
+
 		// If the new transaction is underpriced, don't accept it
 		if pool.priced.Underpriced(tx) {
 			log.Trace("Discarding underpriced transaction", "hash", hash, "gasTipCap", tx.GasTipCap(), "gasFeeCap", tx.GasFeeCap())
 			underpricedTxMeter.Mark(1)
+			stage1Duration = time.Since(stage1Time)
 			return false, txpool.ErrUnderpriced
 		}
 
@@ -831,6 +915,7 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 		// replacements to 25% of the slots
 		if pool.changesSinceReorg > int(pool.config.GlobalSlots/4) {
 			throttleTxMeter.Mark(1)
+			stage1Duration = time.Since(stage1Time)
 			return false, ErrTxPoolOverflow
 		}
 
@@ -842,13 +927,23 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 		if !success {
 			log.Trace("Discarding overflown transaction", "hash", hash)
 			overflowedTxMeter.Mark(1)
+			stage1Duration = time.Since(stage1Time)
 			return false, ErrTxPoolOverflow
 		}
+
+		// stage1 ends, update final duration, increment stage
+		stage1Duration = time.Since(stage1Time)
+		currentStage = 2
+
+		// update stage2 start time
+		stage2Time = time.Now()
 
 		if async {
 			// We're done operating on the `pricedList`. Acquire the lock again
 			// for rest of the operations.
+			lockStart := time.Now()
 			pool.mu.Lock()
+			asyncAddStage2LockWaitTimer.Update(time.Since(lockStart))
 			locked = true
 		}
 
@@ -874,6 +969,7 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 					}
 				}
 				log.Trace("Discarding future transaction replacing pending tx", "hash", hash)
+				stage2Duration = time.Since(stage2Time)
 				return false, ErrFutureReplacePending
 			}
 		}
@@ -890,12 +986,16 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 		}
 	}
 
+	// increment stage, stage2 time already captured above
+	currentStage = 2
+
 	// Try to replace an existing transaction in the pending pool
 	if list := pool.pending[from]; list != nil && list.Contains(tx.Nonce()) {
 		// Nonce already pending, check if required price bump is met
 		inserted, old := list.Add(tx, pool.config.PriceBump)
 		if !inserted {
 			pendingDiscardMeter.Mark(1)
+			stage2Duration = time.Since(stage2Time)
 			return false, txpool.ErrReplaceUnderpriced
 		}
 		// New transaction is better, replace old one
@@ -917,14 +1017,17 @@ func (pool *LegacyPool) add(tx *types.Transaction, async bool) (replaced bool, e
 
 		// Successful promotion, bump the heartbeat
 		pool.beats[from] = time.Now()
+		stage2Duration = time.Since(stage2Time)
 		return old != nil, nil
 	}
 	// New transaction isn't replacing a pending one, push into queue
 	replaced, err = pool.enqueueTx(hash, tx, true)
 	if err != nil {
+		stage2Duration = time.Since(stage2Time)
 		return false, err
 	}
 
+	stage2Duration = time.Since(stage2Time)
 	log.Trace("Pooled new future transaction", "hash", hash, "from", from, "to", tx.To())
 	return replaced, nil
 }
