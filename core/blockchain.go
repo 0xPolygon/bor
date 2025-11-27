5899486a80f24437e5292e4a74269bdedac09079
Angel Valkov <avalkov@polygon.technology>
2025-11-27 16:22:56 +0200

    Dont commit block in serial execution

--- core/blockchain.go
+++ core/blockchain.go
@@ -3224,6 +3224,8 @@ func (bc *BlockChain) insertChainWithWitnesses(chain types.Blocks, setHead bool,
 			return nil, it.index, whitelist.ErrMismatch
 		}
 
+		panic("Wont commit block")
+
 		if !setHead {
 			// Don't set the head, only insert the block
 			_, err = bc.writeBlockWithState(block, receipts, logs, statedb)
