13ec77af412604f62e388d1b73747b457f0db5d2
Manav Darji <manavdarji.india@gmail.com>
2026-01-06 21:49:03 +0530

    fix tests

--- consensus/misc/eip1559/eip1559_test.go
+++ consensus/misc/eip1559/eip1559_test.go
@@ -203,7 +203,6 @@ func TestCalcParentGasTarget(t *testing.T) {
 		}
 		gasTarget := calcParentGasTarget(testConfig, block)
 		expected := block.GasLimit * params.TargetGasPercentagePostDandeli / 100 // because gas target is derived by this protocol parameter
-		fmt.Println("gas target", gasTarget, "expected", expected)
 		require.Equal(t, expected, gasTarget, "case #1: expected gas target = 60 percent of gas limit")
 
 		block = &types.Header{
